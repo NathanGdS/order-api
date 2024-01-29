d9ca1dd00367a27773c42145c6488b91999080ca
Nathan Santos <nathan.santos@finnet.com.br>
2024-01-28 21:49:25 -0300

    fix: has many relationship

--- models/item.model.go
+++ models/item.model.go
@@ -11,11 +11,10 @@ type NewItemRequest struct {
 }
 
 type Item struct {
-	ItemId      string   `gorm:"primaryKey;type:uuid;default:gen_random_uuid()" json:"item_id"`
-	Name        string   `json:"name" validate:"required"`
-	Description string   `json:"description" validate:"optional"`
-	CategoryId  string   `json:"category_id" validate:"required"`
-	Category    Category `gorm:"foreignKey:CategoryId;references:CategoryId" json:"category"`
+	ItemId      string `gorm:"primaryKey;type:uuid;default:gen_random_uuid()" json:"item_id"`
+	Name        string `json:"name" validate:"required"`
+	Description string `json:"description" validate:"optional"`
+	CategoryId  string `json:"category_id" validate:"required"`
 	BaseModels
 }
 
